23401ed4811dbca5ca48442806df448a6f5c6153
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2019-06-28 16:16:55 +0900

    Fix doc

--- R/data.R
+++ R/data.R
@@ -1,10 +1,10 @@
 #' Societies in the WNAI dataset.
 #'
-#' @format A data frame with 172 societies (rows) and 5 variables (columns):
+#' @format A data frame with 172 societies (rows) and 6 variables (columns):
 #' \describe{
 #'     \item{`id`}{D-PLACE-wide id for the society}
 #'     \item{`name`}{name of the society}
-#'     \item{`alt_names`}{alternative names of the society}
+#'     \item{`alt_names`}{alternative names of the society (comma separated)}
 #'     \item{`glottocode`}{code of the most specific Glottolog languoid for
 #'         the society}
 #'     \item{`latitude`}{location of the society in latitude}
@@ -13,10 +13,10 @@
 #' @source
 #' Jorgensen, J. G. (1980) _Western Indians: Comparative Environments,
 #' Languages and Cultures of 172 Western American Indian Tribes_. W. H.
-#' Freeman. 
+#' Freeman.
 #'
 #' Jorgensen, J. G. (1999) Codebook for Western Indians data. _World Cultures_
-#' 10(2): 144-293. 
+#' 10(2): 144-293.
 #'
 #' The dataset in D-PLACE is imported from _World Cultures_ CD-ROM (2011)
 #' using codes described in Jorgensen (1999).
