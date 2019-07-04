a7af808aa2bf29b0c1815bc356ddd60381585b32
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2019-07-04 11:25:36 +0900

    Fix docs

--- R/data.R
+++ R/data.R
@@ -2,6 +2,7 @@ version <- "v2.0.1"
 
 #' Societies in the WNAI dataset.
 #'
+#' @usage wnai::societies
 #' @format A data frame with 172 societies (rows) and 6 variables (columns):
 #' \describe{
 #'     \item{`id`}{D-PLACE-wide id for the society}
@@ -26,15 +27,16 @@ version <- "v2.0.1"
 
 #' Variables in the WNAI dataset.
 #'
+#' @usage wnai::variables
 #' @format A data frame with 429 variables (rows) and 5 attributes (columns):
 #' \describe{
 #'     \item{`id`}{D-PLACE-wide id for the variable}
-#'     \item{`title`}{title of the variable}
+#'     \item{`name`}{name of the variable}
 #'     \item{`type`}{type of the variable, either `cat`egorical, `ord`inal, or
 #'         `cont`inuous}
 #'     \item{`categories`}{categories that can be assigned to the variable
 #'         (comma separated)}
-#'     \item{`definitions`}{definitions of terms used in the `title`}
+#'     \item{`definitions`}{definitions of terms used in the `name` column}
 #' }
 #' @source
 #' Jorgensen, J. G. (1980) _Western Indians: Comparative Environments,
@@ -48,35 +50,17 @@ version <- "v2.0.1"
 #' using codes described in Jorgensen (1999).
 "variables"
 
-#' Data in the WNAI dataset.
+#' Codes for each variable in the WNAI dataset.
 #'
-#' @format A data frame with 172 societies (rows) and 430 variables (columns):
-#' \describe{
-#'     \item{`soc_id`}{D-PLACE-wide id for the society}
-#'     \item{`WNAI8..WNAI438`}{429 variables for each society}
-#' }
-#' @source
-#' Jorgensen, J. G. (1980) _Western Indians: Comparative Environments,
-#' Languages and Cultures of 172 Western American Indian Tribes_. W. H.
-#' Freeman.
-#'
-#' Jorgensen, J. G. (1999) Codebook for Western Indians data. _World Cultures_
-#' 10(2): 144-293.
-#'
-#' The dataset in D-PLACE is imported from _World Cultures_ CD-ROM (2011)
-#' using codes described in Jorgensen (1999).
-"data"
-
-#' Codes in the WNAI dataset.
-#'
-#' @format A nested data frame with 429 pairs of variables and codes (rows):
+#' @usage wnai::codes
+#' @format A nested data frame with 429 pairs of variable id and codes (rows):
 #' \describe{
 #'     \item{`var_id`}{D-PLACE-wide id for the variable}
-#'     \item{`codes`}{codes for each variable:
+#'     \item{`codes`}{codes used in the variable:
 #'         \describe{
-#'             \item{`code`}{codes of the variable in appropriate type}
-#'             \item{`name`}{short description of the codes}
-#'             \item{`description`}{longer description of the codes}
+#'             \item{`code`}{code in the variable}
+#'             \item{`name`}{short description of the code}
+#'             \item{`description`}{longer description of the code}
 #'         }}
 #' }
 #' @source
@@ -90,3 +74,23 @@ version <- "v2.0.1"
 #' The dataset in D-PLACE is imported from _World Cultures_ CD-ROM (2011)
 #' using codes described in Jorgensen (1999).
 "codes"
+
+#' Main data of the WNAI dataset.
+#'
+#' @usage wnai::data
+#' @format A data frame with 172 societies (rows) and 430 variables (columns):
+#' \describe{
+#'     \item{`soc_id`}{D-PLACE-wide id for the society}
+#'     \item{`WNAI8..WNAI438`}{values of 429 variables for the society}
+#' }
+#' @source
+#' Jorgensen, J. G. (1980) _Western Indians: Comparative Environments,
+#' Languages and Cultures of 172 Western American Indian Tribes_. W. H.
+#' Freeman.
+#'
+#' Jorgensen, J. G. (1999) Codebook for Western Indians data. _World Cultures_
+#' 10(2): 144-293.
+#'
+#' The dataset in D-PLACE is imported from _World Cultures_ CD-ROM (2011)
+#' using codes described in Jorgensen (1999).
+"data"
